1318650987b1fd2c0863fd8e692e8d7b92e0a3bd
Cuneyt YENER <cuneytyener@gmail.com>
2019-08-14 11:00:40 +0100

    Add project files.

---
+++ image_pro/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace image_pro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "asd",
+            "asd",
+            "asd",
+            "a",
+            "dsas",
+            "da",
+            "sd",
+            "asd",
+            "asd",
+            "as",
+            "da",
+            "sd",
+            "ads",
+            "asd",
+            "as",
+            "dda",
+            "sda",
+            "sd",
+            "asd",
+            "asd",
+            "a",
+            "sda",
+            "sd",
+            "asd",
+            "a",
+            "sda",
+            "sd",
+            "asd",
+            "as",
+            "da",
+            "sd",
+            "asd",
+            "asd",
+            "a",
+            "ds",
+            "234",
+            "svb",
+            "snb",
+            "sfh",
+            "hjfg"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 97);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(776, 304);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(360, 57);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(213, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.ProgressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ image_pro/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using ImageProcessor.Imaging;
+using ImageProcessor.Imaging.Filters.EdgeDetection;
+using ImageProcessor.Imaging.Filters.Photo;
+using ImageProcessor;
+
+
+
+
+namespace image_pro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //
+            using (ImageFactory resNesnesi = new ImageFactory(preserveExifData: false))
+            {
+                // resim yüklemesi.
+                Size rsize = new Size(520,520);
+
+                ResizeLayer rs = new ResizeLayer(rsize, ResizeMode.BoxPad);
+                resNesnesi.Load("./sc.jfif").Resize(rs).BackgroundColor(Color.White).Save("./aa.jpg");
+
+                
+
+
+
+
+
+            }
+
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+              
+        }
+    }
+}
